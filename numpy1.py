bf8b198ce9e1adbb95833b9fa3295d202c9327d3
Aryanmahakur <mahakuraryan@gmail.com>
2025-02-28 20:20:40 +0530

    uf

--- numpy1.py
+++ numpy1.py
@@ -61,10 +61,17 @@ import numpy as np
 # there are more functions
 
 np1=np.array([0,1,2,3,4,5])
-# cretae a view
-np2=np1.view()
-print(f'orignal np1{np1}')
-print(f'view np2{np2}')
+# # cretae a view
+# np2=np1.view()
+# print(f'orignal np1{np1}')
+# print(f'view np2{np2}')
+# np1[0]=42
+# print(f'changed np1{np1}')
+# print(f'original np2{np2}')
+#create a copy
+np2=np1.copy()
+print(f'original np1{np1}')
+print(f'copy np2{np2}')
 np1[0]=42
 print(f'changed np1{np1}')
 print(f'original np2{np2}')
