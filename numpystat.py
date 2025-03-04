73de8e69f807004792784338bc87c8da883fde72
Aryanmahakur <mahakuraryan@gmail.com>
2025-03-04 19:43:29 +0530

    uf

---
+++ numpystat.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+#mean
+np1=np.array([1, 2, 3, 4, 5])
+print(np.mean(np1))
+#median
+np2=np.array([1, 2, 3, 4, 5])
+print(np.median(np2))
+#standard deviation
+np3=np.array([50,60,70,80,90])
+print(np.std(np3))
